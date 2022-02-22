342bc6e844af6d75f21225dd7f47d938472d10ca
Sankhyesh Singh Thakur <sankhyesh3@gmail.com>
2022-02-22 19:44:51 +0530

    Update 232. Implement Queue using Stacks.cpp
    https://leetcode.com/problems/implement-queue-using-stacks/

--- Stack And Queue/232. Implement Queue using Stacks.cpp
+++ Stack And Queue/232. Implement Queue using Stacks.cpp
@@ -1,45 +1,98 @@
+// class MyQueue {
+    
+// public:
+//     stack<int> s1,s2;
+//     MyQueue() {
+        
+//     }
+    
+//     void reverse(int size, stack<int> &s1, stack<int> &s2){
+//         for(int i = 0; i<size; i++){
+//             s2.push(s1.top());
+//             s1.pop();
+//         }
+        
+//     }
+    
+//     void push(int x) {
+//         reverse(s1.size(), s1, s2);
+//         s2.push(x);
+//         reverse(s2.size(), s2, s1);
+//     }
+    
+//     int pop() {
+//         if(s1.size() == 0){
+//             return -1;
+//         }
+//         int x = s1.top();
+//         s1.pop();
+//         return x;
+//     }
+    
+//     int peek() {
+//         if(s1.size() == 0){
+//             return -1;
+//         }
+//         return s1.top();
+//     }
+    
+//     bool empty() {
+//         return s1.size() == 0;
+//     }
+// };
 
-
+// amortized optimal solution 
 class MyQueue {
     
 public:
-    stack<int> s1,s2;
+    stack<int> input, output;
     MyQueue() {
         
     }
     
-    void reverse(int size, stack<int> &s1, stack<int> &s2){
-        for(int i = 0; i<size; i++){
-            s2.push(s1.top());
-            s1.pop();
-        }
-        
-    }
-    
     void push(int x) {
-        reverse(s1.size(), s1, s2);
-        s2.push(x);
-        reverse(s2.size(), s2, s1);
+        input.push(x);
     }
     
     int pop() {
-        if(s1.size() == 0){
-            return -1;
+        if(output.empty() == false){
+            int x = output.top();
+            output.pop();
+            return x;
+        }else{
+            while(input.empty() == false){
+                output.push(input.top());
+                input.pop();
+            }
+            int x = output.top();
+            output.pop();
+            return x;
         }
-        int x = s1.top();
-        s1.pop();
-        return x;
     }
     
     int peek() {
-        if(s1.size() == 0){
-            return -1;
+        if(output.empty() == false){
+            return output.top();
+        }else{
+            while(input.empty() == false){
+                output.push(input.top());
+                input.pop();
+            }
+            return output.top();
         }
-        return s1.top();
     }
     
     bool empty() {
-        return s1.size() == 0;
+        if(output.empty() == false){
+            return output.size()==0;
+        }else{
+            while(input.empty() == false){
+                output.push(input.top());
+                input.pop();
+            }
+            return output.size()==0;
+        }
+        
     }
 };
 
@@ -52,6 +105,11 @@ public:
  * bool param_4 = obj->empty();
  */
 
-
-
-// $$$$$$$$$$ Optimal solution &&&&&
+// /**
+//  * Your MyQueue object will be instantiated and called as such:
+//  * MyQueue* obj = new MyQueue();
+//  * obj->push(x);
+//  * int param_2 = obj->pop();
+//  * int param_3 = obj->peek();
+//  * bool param_4 = obj->empty();
+//  */
