7fea974223fa15def0b9a4ccbb68ff92f1b8191e
Sankhyesh Singh Thakur <sankhyesh3@gmail.com>
2022-03-06 16:08:21 +0530

    Update 94. Binary Tree Inorder Traversal.cpp
    https://leetcode.com/problems/binary-tree-inorder-traversal/submissions/

--- BinaryTree/94. Binary Tree Inorder Traversal.cpp
+++ BinaryTree/94. Binary Tree Inorder Traversal.cpp
@@ -9,19 +9,58 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+/// Recursive solutions
+// class Solution {
+// public:
+//     void inorder(TreeNode* root, vector<int> &res){
+//         if(root != NULL){
+//         inorder(root -> left, res);
+//         res.push_back(root->val);
+//         inorder(root -> right, res);
+//         }
+//     }
+    
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int> res;
+//         inorder(root, res);
+//         return res;
+//     }
+// };
+
+
+// iteratie solutions
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int> &res){
-        if(root != NULL){
-        inorder(root -> left, res);
-        res.push_back(root->val);
-        inorder(root -> right, res);
-        }
-    }
     
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> res;
-        inorder(root, res);
+        stack<TreeNode *> st;
+        TreeNode* node = root;
+        while(true){
+            if(node != NULL){
+                st.push(node);
+                node = node->left;
+            }else{ // if node is null
+                if(st.empty() == true)
+                    break;
+                
+                node = st.top();
+                st.pop();
+                res.push_back(node->val); 
+                node = node->right;
+            }
+        }
+        
         return res;
     }
 };
+
+
+
+
+
+
+
+
+
