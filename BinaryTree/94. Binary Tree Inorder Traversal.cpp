d8480f10e6d695b5dff2c8f1ff22a571c0b6c0e3
Sankhyesh Singh Thakur <sankhyesh3@gmail.com>
2022-03-06 18:54:20 +0530

    Update 94. Binary Tree Inorder Traversal.cpp
    https://leetcode.com/problems/binary-tree-preorder-traversal/

--- BinaryTree/94. Binary Tree Inorder Traversal.cpp
+++ BinaryTree/94. Binary Tree Inorder Traversal.cpp
@@ -10,57 +10,55 @@
  * };
  */
 
-/// Recursive solutions
+// REcursive Function
 // class Solution {
 // public:
-//     void inorder(TreeNode* root, vector<int> &res){
+//     void preorder( TreeNode* root, vector<int> &res){
 //         if(root != NULL){
-//         inorder(root -> left, res);
-//         res.push_back(root->val);
-//         inorder(root -> right, res);
+//             res.push_back(root->val);
+//             preorder(root->left, res);
+//             preorder(root->right, res);
 //         }
 //     }
     
-//     vector<int> inorderTraversal(TreeNode* root) {
+//     vector<int> preorderTraversal(TreeNode* root) { 
 //         vector<int> res;
-//         inorder(root, res);
+//         preorder(root, res);
 //         return res;
 //     }
 // };
 
-
-// iteratie solutions
+// iterative method
 class Solution {
 public:
     
-    vector<int> inorderTraversal(TreeNode* root) {
+    vector<int> preorderTraversal(TreeNode* root) { 
         vector<int> res;
-        stack<TreeNode *> st;
-        TreeNode* node = root;
-        while(true){
-            if(node != NULL){
-                st.push(node);
-                node = node->left;
-            }else{ // if node is null
-                if(st.empty() == true)
-                    break;
-                
-                node = st.top();
-                st.pop();
-                res.push_back(node->val); 
-                node = node->right;
-            }
+        if(!root){
+            return res;
         }
-        
+        stack<TreeNode *> st;
+        st.push(root);
+        TreeNode* node = root;
+        while(!st.empty()){
+            
+            node = st.top();
+            st.pop();
+            res.push_back(node->val);
+            
+            if(node ->right){// push the right first because at 
+                // the time of the pop left will come first LIFO
+                // node = node ->right;
+                st.push(node -> right);
+            }
+            
+            if(node->left){
+                // node = node->left;
+                st.push(node->left);
+            }
+            
+            
+        }
         return res;
     }
 };
-
-
-
-
-
-
-
-
-
