83ada126416fe10659e0008f99e1f7cc890f0fda
Sankhyesh Singh Thakur <sankhyesh3@gmail.com>
2022-03-23 23:29:12 +0530

    Update Path to Given Node.cpp
    https://www.interviewbit.com/problems/path-to-given-node/

--- BinaryTree/Path to Given Node.cpp
+++ BinaryTree/Path to Given Node.cpp
@@ -9,21 +9,27 @@
  */
 
 bool getPath(TreeNode* root, vector<int> &ans, int B){
-    //if root is null then return false bcz 
+    //if root is null then return false bcz we dont have ans
+    // in that line  so return false
     if(!root){
         return false;
     }
 
+    // push that node to the ans
     ans.push_back(root->val);
 
 
     if(root->val == B){
         return true;
     }
-    if(getPath(root->left, ans, B) || getPath(root->right, ans, B)){
+
+    if(getPath(root->left, ans, B) || getPath(root->right, ans, B)){ // if till the end u can 
+    // found the path then return the true
         return true;
     }
 
+    // if the path is not in that branch then pop the 
+    // element return false 
     ans.pop_back();
 
     return false;
